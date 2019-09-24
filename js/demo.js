8d1926eb1642777eee5e83e6fb69e78b7aa44144
hoangvan1254 <phamthanh121384@gmail.com>
2019-09-24 21:59:04 +0700

    initial commit

---
+++ js/demo.js
@@ -0,0 +1,7 @@
+var svgContainer = document.getElementById('bm');
+var animItem = bodymovin.loadAnimation({
+  wrapper: svgContainer,
+  animType: 'svg',
+  loop: true,
+  path: '../data.json'
+});// JavaScript Document
